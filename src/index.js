10f6780d12f03a270b93c2759b6767ed1cddbe62
Luis Monagas <monagasweb2020@gmail.com>
2023-03-30 13:44:23 -0400

    Capture user input by console

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-/** @format */
-
 import { promptQuestion } from '#Lib/promptQuestion.js';
 
 (async () => {
-    // 1ª Capturar la entrada del usuario por consola.
-    const userAnswer = await promptQuestion('Introduce una operación ');
+    // 1ª Capturar la entrada del usuario por consola
+    const userAnswer = await promptQuestion('Introduzca la operación ');
     console.log(userAnswer);
-    // 2ª Validar la entrada y separar las partes de la misma en operandos y operador
-
-    // 3ª Realizar la operación
-
-    // 4ª Mostrar reslutado por consola
 })();
