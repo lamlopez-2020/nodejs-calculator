bc008bf842a2520868d87f30489ec850575d1051
Luis Monagas <monagasweb2020@gmail.com>
2023-04-11 22:56:47 -0400

    Ready latest refactor

---
+++ src/bootstrap.js
@@ -0,0 +1,54 @@
+import { promptQuestion } from '#Lib/promptQuestion.js';
+import { BYNARY_OPERATORS } from '#Constants/operators.js';
+import { getOperator } from '#Lib/getOperator.js';
+import { InvalidInputError, InvalidOperation } from '#Errors/errors.js';
+import { getBinaryOperating, getSingleOperating } from '#Lib/getOperatings.js';
+import { OPERATIONS } from '#Constants/operations.js';
+
+export const bootstrap = async () => {
+    try {
+        // 1ª Create user input by console
+        const userAnswer = await promptQuestion('Introduce la operación: ');
+
+        // 2ª Console input validation and separate the parts of them into operands and operator
+
+        const standarizeInput = userAnswer.trim();
+
+        if (!standarizeInput) throw new InvalidInputError();
+
+        if (!standarizeInput) throw new InvalidInputError();
+
+        if (standarizeInput === 'exit') return true;
+
+        const operator = getOperator(standarizeInput);
+
+        const splittedInput = standarizeInput.split(operator);
+
+        let firstOperating, secondOperating;
+
+        if (BYNARY_OPERATORS.includes(operator))
+            [firstOperating, secondOperating] =
+                getBinaryOperating(splittedInput);
+        else [firstOperating] = getSingleOperating(splittedInput);
+
+        // 3ª Perform the operrors module creationeration
+
+        const result = OPERATIONS[operator](firstOperating, secondOperating);
+
+        const roundedResult = Number(Math.round(result + 'e+5') + 'e-5');
+
+        if (isNaN(roundedResult) || !isFinite(roundedResult))
+            throw new InvalidOperation();
+        else console.log(`El resultado es: ${roundedResult}\n`);
+    } catch (error) {
+        if (
+            error instanceof InvalidInputError ||
+            error instanceof InvalidOperation
+        )
+            console.log(`${error.message}\n`);
+        else
+            console.log(
+                `Error no Controlado : ${error.message}. Stack: ${error.stack}\n`
+            );
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,50 +1,14 @@
-import { promptQuestion } from '#Lib/promptQuestion.js';
-import { BYNARY_OPERATORS } from '#Constants/operators.js';
-import { getOperator } from '#Lib/getOperator.js';
-import { InvalidInputError, InvalidOperation } from '#Errors/errors.js';
-import { getBinaryOperating, getSingleOperating } from '#Lib/getOperatings.js';
-import { OPERATIONS } from '#Constants/operations.js';
+import { closeInterfase } from '#Lib/promptQuestion.js';
+import { bootstrap } from './bootstrap.js';
 
-(async () => {
-    try {
-        // 1ª Create user input by console
-        const userAnswer = await promptQuestion('Introduce la operación: ');
+const main = async () => {
+    let stopFlag = false;
 
-        // 2ª Console input validation and separate the parts of them into operands and operator
-
-        const standarizeInput = userAnswer.trim();
-
-        if (!standarizeInput) throw new InvalidInputError();
-
-        const operator = getOperator(standarizeInput);
-
-        const splittedInput = standarizeInput.split(operator);
-
-        let firstOperating, secondOperating;
-
-        if (BYNARY_OPERATORS.includes(operator))
-            [firstOperating, secondOperating] =
-                getBinaryOperating(splittedInput);
-        else [firstOperating] = getSingleOperating(splittedInput);
-
-        // 3ª Perform the operation
-
-        const result = OPERATIONS[operator](firstOperating, secondOperating);
-
-        const roundedResult = Number(Math.round(result + 'e+5') + 'e-5');
-
-        if (isNaN(roundedResult) || !isFinite(roundedResult))
-            throw new InvalidOperation();
-        else console.log(roundedResult);
-    } catch (error) {
-        if (
-            error instanceof InvalidInputError ||
-            error instanceof InvalidOperation
-        )
-            console.log(error.message);
-        else
-            console.log(
-                `Error no Controlado : ${error.message}. Stack: ${error.stack}`
-            );
+    while (!stopFlag) {
+        const flag = await bootstrap();
+        stopFlag = flag;
     }
-})();
+    closeInterfase();
+};
+
+main();
