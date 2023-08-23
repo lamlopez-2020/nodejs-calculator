45e0fa8125459fd5f8ca9e4b22eeec2583cacba0
lamlopez-2020 <monagasweb2020@gmail.com>
2023-08-23 03:52:16 -0400

    Correcci{on del array ALL_OPERATORS

--- src/constants/operators.js
+++ src/constants/operators.js
@@ -1,4 +1,4 @@
 export const BINARY_OPERATORS = ['+', '-', '*', '/', '^'];
 export const SINGLE_OPERATORS = ['log', 'sqrt'];
 
-export const ALL_OPERATORS = [...BINARY_OPERATORS, SINGLE_OPERATORS];
+export const ALL_OPERATORS = [...BINARY_OPERATORS, ...SINGLE_OPERATORS];
